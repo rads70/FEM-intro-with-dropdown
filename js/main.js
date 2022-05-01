02c0964661a161acb67fe99f012013a2bad89013
Richard Suttie <richard@suttie.no>
2022-05-01 21:54:58 +0200

    first draft

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+document.body.addEventListener("click", function (event) {
+   if (event.target.classList.contains("dropdown--link")) {
+      event.target.nextElementSibling.classList.toggle("active");
+      event.target.firstElementChild.classList.toggle("arrow_active");
+   }
+});
 function closeNav() {
-  document.querySelector(".navbar").classList.remove("sidebar__open");
-  document.body.style.backgroundColor = "rgba(255,255,255)";
+   document.querySelector(".navbar").classList.remove("sidebar__open");
+   document.body.style.backgroundColor = "var(--almostWhite)";
 }
 function openNav() {
-  document.querySelector(".navbar").classList.add("sidebar__open");
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+   document.querySelector(".navbar").classList.add("sidebar__open");
+   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
